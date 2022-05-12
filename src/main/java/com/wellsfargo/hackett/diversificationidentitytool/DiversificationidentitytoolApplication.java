12d37afa191160c6d00bc4c166c934e762948afa
Ankit Agarwal <ankitagarwal@Ankits-MacBook-Air.local>
2022-05-12 08:00:36 +0530

    boiler plate change:ww

---
+++ src/main/java/com/wellsfargo/hackett/diversificationidentitytool/DiversificationidentitytoolApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.hackett.diversificationidentitytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiversificationidentitytoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiversificationidentitytoolApplication.class, args);
+	}
+
+}
